f21612a469fc3375be654ea6ae115ce77d433ca5
veguinho <rafaelvr@al.insper.edu.br>
2019-11-27 14:41:38 -0300

    uc

--- UC_ULA.vhd
+++ UC_ULA.vhd
@@ -16,8 +16,45 @@ entity UC_ULA is
 end entity;
 
 architecture bhv of UC_ULA is	
-    signal ALUop_s : STD_LOGIC_VECTOR(ALU_OP_WIDTH-1 DOWNTO 0);
+    	signal d_funct : std_logic_vector(CTRL_ALU_WIDTH-1 downto 0);
+		signal saida : std_logic_vector(CTRL_ALU_WIDTH-1 downto 0);
+
 begin
 
+	d_funct <= ulaCtrlAdd when (funct = functADD) else
+				  ulaCtrlSub when (funct = functSUB) else
+				  ulaCtrlAnd when (funct = functAND) else
+				  ulaCtrlOr when (funct = functOR) else
+				  ulaCtrlSlt when (funct = functSLT) else
+				  "1111";
+				  
+--		when functADD => d_funct <= ulaCtrlAdd;
+--		when functSUB => d_funct <= ulaCtrlSub;
+--		when functAND => d_funct <= ulaCtrlAnd;
+--		when functOR => d_funct <= ulaCtrlOr;
+--		when functSLT => d_funct <= ulaCtrlSlt;
+--		when others => d_funct <= "1111";
+--		 
+--	 end case;
+
+	 saida <= d_funct when (ALUop = readFunctULA) else
+				  ulaCtrlAdd when (ALUop = aluOpAdd) else
+				  ulaCtrlSub when (ALUop = aluOpSub) else
+				  ulaCtrlAnd when (ALUop = aluOpAnd) else
+				  ulaCtrlOr when (ALUop = aluOpOr) else
+				  ulaCtrlSlt when (ALUop = aluOpSlt) else
+				  "1111";
+	 
+--	 case ALUop is
+--		when readFunctULA => saida <= d_funct; 
+--		when aluOpAdd => saida <= ulaCtrlAdd;
+--		when aluOpSub => saida <= ulaCtrlSub;
+--		when aluOpAnd => saida <= ulaCtrlAnd;
+--		when aluOpOr =>  saida <= ulaCtrlOr;
+--		when aluOpSlt =>  saida <= ulaCtrlSlt;
+--		when others => saida <= "1111";
+--	 end case;
+ 
+	 ALUctr <= saida;
 
 end bhv;
--- uc.vhd
+++ uc.vhd
@@ -16,6 +16,18 @@ end entity;
 
 architecture bhv of uc is
 begin
-    process(opcode)
+		pontosDeControle <= ctrlTipoR when (opcode = opCodeTipoR) else
+								  ctrlTipoLW when (opcode = opCodeLW) else
+								  ctrlTipoSW when (opcode = opCodeSW) else
+								  ctrlTipoBEQ when (opcode = opCodeBEQ) else
+								  ctrlTipoJ when (opcode = opCodeTipoJ) else
+								  "00000000000";
+
+--		when	opCodeTipoR => pontosDeControle <= ctrlTipoR;
+--		when	opCodeLW => pontosDeControle <= ctrlTipoLW;
+--		when	opCodeSW => pontosDeControle <= ctrlTipoSW;
+--		when	opCodeBEQ => pontosDeControle <= ctrlTipoBEQ;
+--		when opCodeTipoJ => pontosDeControle <= ctrlTipoJ;
+--		when others => pontosDeControle <= "00000000";
 
 end bhv;
